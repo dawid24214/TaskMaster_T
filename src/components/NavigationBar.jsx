8dc63d08cef861d0d698f3586125926d9d720b6f
dawid24214 <155809372+dawid24214@users.noreply.github.com>
2025-03-26 21:11:27 +0100

    eddition
    lucie react

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,10 +1,11 @@
 import {Link} from "react-router-dom";
+import {Home, List, PlusCircle, BarChart, User} from 'luci'
 import React from "react";
 import '../style/_navigationbar.scss';
   export const NavigationBar = () =>{
     return(
         <>
-            <div className='navigation-app'>
+            <div className='navigation-bar'>
                 <li>Home</li>
                 <li>Tasks</li>
                 <li>Add Button</li>
